a7b3cd5b53cd04de64ae7bc22ef0d0601225fde3
RikWijtman <1045768@hr.nl>
2023-06-14 12:57:34 +0200

    frederik?

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,12 +3,14 @@ import mainCharacterImage from '../images/mainCharacter.png'
 import ghoulImage from '../images/ghosts/ghoul.png'
 import play from "../images/play.png"
 import lobby from "../images/rooms/lobby.png"
+import Frederik from "../images/rooms/Frederik.png"
 
 const Resources = {
     mainCharacter: new ImageSource(mainCharacterImage),
     ghoul: new ImageSource(ghoulImage),
     Play: new ImageSource(play),
     Lobby: new ImageSource(lobby),
+    Frederik: new ImageSource(Frederik)
 
 }
 
@@ -16,6 +18,7 @@ const ResourceLoader = new Loader([Resources.mainCharacter,
     Resources.ghoul, 
     Resources.Play, 
     Resources.Lobby,
+    Resources.Frederik,
 
 ])
 
