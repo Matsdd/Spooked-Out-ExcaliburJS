2de0a178372747490d07638c142fdb8edc628305
Matsdd <Matsdoornhein04@gmail.com>
2023-06-14 11:57:19 +0200

    achtergrond

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,19 +1,22 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import mainCharacterImage from '../images/mainCharacter.png'
 import ghoulImage from '../images/ghosts/ghoul.png'
-
 import play from "../images/play.png"
-
+import lobby from "../images/rooms/lobby.png"
 
 const Resources = {
     mainCharacter: new ImageSource(mainCharacterImage),
     ghoul: new ImageSource(ghoulImage),
     Play: new ImageSource(play),
+    Lobby: new ImageSource(lobby),
+
 }
-const ResourceLoader = new Loader([Resources.mainCharacter, Resources.ghoul, Resources.Play, ])
-    
 
+const ResourceLoader = new Loader([Resources.mainCharacter, 
+    Resources.ghoul, 
+    Resources.Play, 
+    Resources.Lobby,
 
-
+])
 
 export { Resources, ResourceLoader }
--- src/js/rooms/lobbyRoom.js
+++ src/js/rooms/lobbyRoom.js
@@ -1,6 +1,7 @@
 import { Actor, Engine, Vector, TextAlign, Scene } from "excalibur"
 import { mainCharacter } from '../mainCharacter.js'
 import { ghoul } from '../enemies/ghoul.js'
+import { Resources } from '../resources.js'
 
 export class lobbyRoom extends Scene {
 
@@ -9,6 +10,12 @@ export class lobbyRoom extends Scene {
     }
     
     onInitialize(engine) {
+        const Lobby = new Actor();
+        Lobby.graphics.use(Resources.Lobby.toSprite());
+        Lobby.pos = new Vector(767, 430);
+        Lobby.scale = new Vector(0.8, 0.8);
+        this.add(Lobby);
+
 
         let Sjaak = new mainCharacter()
         this.add(Sjaak)
