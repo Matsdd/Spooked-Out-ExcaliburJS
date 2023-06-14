0f99dd4971fb8310958ea835c46443d318f78b6a
lindsey <lindseytaak123@gmail.com>
2023-06-14 10:17:42 +0200

    resources maincharacter geadd

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,24 +1,11 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import ExploderImage from '../sprites/robots/ExploderI.png'
-import DamageExploderImage from '../sprites/robots/ExploderIDamage.png'
+import mainCharacterImage from '../images/mainCharacter.png'
 
 
 
 const Resources = {
-    Exploder: new ImageSource(ExploderImage),
-    DamageExploder: new ImageSource(DamageExploderImage),
-    LaserBingus: new ImageSource(LaserBingusImage),
-    Fortification: new ImageSource(FortificationImage),
-    Laser: new ImageSource(LaserImage),
-    Background: new ImageSource(BackgroundImage),
-    Explosion: new ImageSource(ExplosionImage),
-    Lane: new ImageSource(laneImage),
-    CurrencyCounter: new ImageSource(currencyCounterImage),
-    Settings: new ImageSource(SettingsImage),
-    Selection: new ImageSource(selectionImage)
+    mainCharacter: new ImageSource(mainCharacterImageImage),
 }
-const ResourceLoader = new Loader([Resources.DamageExploder, Resources.Exploder, Resources.LaserBingus, 
-    Resources.Fortification, Resources.Laser, Resources.Background, Resources.Explosion, Resources.Lane,
-    Resources.CurrencyCounter, Resources.Settings, Resources.Selection, ])
+const ResourceLoader = new Loader([Resources.mainCharacter, ])
 
 export { Resources, ResourceLoader }
