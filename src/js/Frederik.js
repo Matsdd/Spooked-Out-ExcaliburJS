e4fa715852c7cdac81cbf665b75453217d3bd2aa
RikWijtman <1045768@hr.nl>
2023-06-14 13:01:33 +0200

    frederik?

--- src/js/Frederik.js
+++ src/js/Frederik.js
@@ -1,6 +1,15 @@
 import { Actor, Engine, Vector, Timer } from "excalibur"
-import { Explosion } from './explosion'
+import { Resources, ResourceLoader } from './resources.js'
 
 export class Enemy extends Actor{
-    
+    constructor() {
+        super({
+            width: Resources.mainCharacter.width/2,
+            height: Resources.mainCharacter.height/2,
+        })
+    }
+
+    onInitialize() {
+        this.graphics.use(Resources.Frederik.toSprite())
+    }
 }
