ca42b1ac5ff560f20f40b865b13ce75a35148e17
RikWijtman <1045768@hr.nl>
2023-06-16 11:18:15 +0200

    jippie collisions

--- src/js/mainCharacter.js
+++ src/js/mainCharacter.js
@@ -5,11 +5,16 @@ import { bullet } from './bullet.js'
 import { room } from './rooms/room.js'
 import { settingsMenu } from './menu/settingsmenu'
 import { Barrier } from './ui/barrier.js'
+import { BarrierChecker } from './ui/playerBarrierChecker.js'
 
 import * as ex from 'excalibur'
  
 export class mainCharacter extends Actor {
   barrierTarget
+  ableUp
+  ableDown
+  ableRight
+  ableLeft
   constructor() {
     super({
       width: Resources.mainCharacter.width/1.6,
@@ -53,6 +58,37 @@ export class mainCharacter extends Actor {
       currentScene.add(Bullet);
     });
   }
+
+  const areaCheckerUp = new BarrierChecker(10,10,55,10,this,'up')
+  areaCheckerUp.on('precollision', (event) => {
+    if (event.other instanceof Barrier) {
+      this.ableUp = 0
+    }
+  })
+  Engine.currentScene.add(areaCheckerUp)
+  const areaCheckerDown = new BarrierChecker(10,10,55,10,this,'down')
+  areaCheckerDown.on('precollision', (event) => {
+    if (event.other instanceof Barrier) {
+      this.ableDown = 0
+    }
+  })
+  Engine.currentScene.add(areaCheckerDown)
+  Engine.currentScene.add(areaCheckerUp)
+  const areaCheckerRight = new BarrierChecker(10,10,10,55,this,'right')
+  areaCheckerRight.on('precollision', (event) => {
+    if (event.other instanceof Barrier) {
+      this.ableRight = 0
+    }
+  })
+  Engine.currentScene.add(areaCheckerRight)
+  Engine.currentScene.add(areaCheckerUp)
+  const areaCheckerLeft = new BarrierChecker(10,10,10,55,this,'left')
+  areaCheckerLeft.on('precollision', (event) => {
+    if (event.other instanceof Barrier) {
+      this.ableLeft = 0
+    }
+  })
+  Engine.currentScene.add(areaCheckerLeft)
 }
 
   onPreUpdate(Engine) {
@@ -62,42 +98,62 @@ export class mainCharacter extends Actor {
     }
   }
 
+  goPosition(barrier,type) {
+    //voor barriercollision(:
+    if (type == 'up') {
+      barrier.pos.x = this.pos.x
+      barrier.pos.y = this.pos.y - 30
+    }
+    if (type == 'down') {
+      barrier.pos.x = this.pos.x
+      barrier.pos.y = this.pos.y + 30
+    }
+    if (type == 'right') {
+      barrier.pos.x = this.pos.x + 30
+      barrier.pos.y = this.pos.y
+    }
+    if (type == 'left') {
+      barrier.pos.x = this.pos.x - 30
+      barrier.pos.y = this.pos.y
+    }
+  }
+
   moveRight() {
-    this.vel.x = this.speed;
+    this.vel.x = this.speed * this.ableRight;
      
   }
 
   moveLeft() {
-    this.vel.x = -this.speed;
+    this.vel.x = -this.speed * this.ableLeft;
     
   }
 
   moveUp() { 
-   if (this.isMovingRight) {
-    this.vel.x = this.speed/1.5;
-    this.vel.y = -this.speed/1.5;
-    
-  } else if (this.isMovingLeft) {
-    this.vel.x = -this.speed/1.5;
-    this.vel.y = -this.speed/1.5;
-    
-  } else {
-    this.vel.y = -this.speed;
-    
-  }
+    if (this.isMovingRight) {
+      this.vel.x = this.speed/1.5 * this.ableRight;
+      this.vel.y = -this.speed/1.5 * this.ableUp;
+      
+    } else if (this.isMovingLeft) {
+      this.vel.x = -this.speed/1.5 * this.ableLeft;
+      this.vel.y = -this.speed/1.5 * this.ableUp;
+      
+    } else {
+      this.vel.y = -this.speed * this.ableUp;
+      
+    }
 }
 
   moveDown() { 
     if (this.isMovingRight) {
-        this.vel.x = this.speed/1.5;
-        this.vel.y = this.speed/1.5;
+        this.vel.x = this.speed/1.5 * this.ableRight;
+        this.vel.y = this.speed/1.5 * this.ableDown;
         
       } else if (this.isMovingLeft) {
-        this.vel.x = -this.speed/1.5;
-        this.vel.y = this.speed/1.5;
+        this.vel.x = -this.speed/1.5 * this.ableLeft;
+        this.vel.y = this.speed/1.5 * this.ableDown;
        
       } else {
-        this.vel.y = this.speed;
+        this.vel.y = this.speed * this.ableDown;
        
       }
   }
@@ -180,6 +236,11 @@ export class mainCharacter extends Actor {
       this.moveDown();
     }
 
+    this.ableUp = 1
+    this.ableDown = 1
+    this.ableLeft = 1
+    this.ableRight = 1
+
     const mouseX = engine.input.pointers.primary.lastWorldPos.x;
     const mouseY = engine.input.pointers.primary.lastWorldPos.y;
     const direction = new Vector(mouseX, mouseY).sub(this.pos);
@@ -187,8 +248,5 @@ export class mainCharacter extends Actor {
     if (direction.distance() > 0) {
       this.rotation = direction.toAngle() + Math.PI / 2;
     }
-
   }
-
-
 }
--- src/js/ui/playerBarrierChecker.js
+++ src/js/ui/playerBarrierChecker.js
@@ -3,16 +3,19 @@ import * as ex from 'excalibur'
 import {Resources} from "../resources.js"
 
 export class BarrierChecker extends Actor {
-
-    constructor(x,y,width,height) {
+    player
+    type
+    constructor(x,y,width,height,player,type) {
         super({width:Resources.Barrier.width, height:Resources.Barrier.height})
         this.graphics.use(Resources.Barrier.toSprite())
         this.pos = new Vector(x,y)
         this.scale = new Vector(width/512, height/512)
-        this.graphics.opacity = 0
+        this.graphics.opacity = 1
+        this.player = player
+        this.type = type
     }
 
     onPreUpdate() {
-        
+        this.player.goPosition(this,this.type)
     }
 }
