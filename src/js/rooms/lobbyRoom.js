9d8d1f1c4a6cee066ff9ebe64b565eeabcd76092
RikWijtman <1045768@hr.nl>
2023-06-16 13:36:47 +0200

    pollroom barriers

--- src/js/rooms/lobbyRoom.js
+++ src/js/rooms/lobbyRoom.js
@@ -10,10 +10,9 @@ import { Tp } from '../ui/tp.js'
 import { room } from './room.js'
 
 export class lobbyRoom extends room {
-    //roomBackground = Resources.Lobby
-    roomBackground = Resources.Pool
+    roomBackground = Resources.Lobby
     spawnBarriers() {
-        /*const barriertopleft = new Barrier(205,215,410,430)
+        const barriertopleft = new Barrier(205,215,410,430)
         this.add(barriertopleft)
         const barriertop = new Barrier(980,40,1150,80)
         this.add(barriertop)
@@ -27,31 +26,8 @@ export class lobbyRoom extends room {
         this.add(barrierfridge2)
         
         const teleporter = new Tp(1290,80,90,20, this.game)
-        this.add(teleporter)*/
-
-        const barrierleft = new Barrier(154,430,308,860)
-        this.add(barrierleft)
-        const barrierright = new Barrier(1390,430,308,860)
-        this.add(barrierright)
-        const barriertop = new Barrier(765,48,1000,96)
-        this.add(barriertop)
-
-        const barrierpoolleft1 = new Barrier(550,350,20,180)
-        this.add(barrierpoolleft1)
-        const barrierpoolleft2 = new Barrier(550,660,20,180)
-        this.add(barrierpoolleft2)
-        const barrierpoolright1 = new Barrier(1020,350,20,180)
-        this.add(barrierpoolright1)
-        const barrierpoolright2 = new Barrier(1020,660,20,180)
-        this.add(barrierpoolright2)
-
-        
-        const barrierpooltop1 = new Barrier(400,800,180,20)
-        this.add(barrierpooltop1)
-        const barrierpoolbottom2 = new Barrier(1020,660,20,180)
-        this.add(barrierpoolbottom2)
-        
-        const teleporter = new Tp(770,90,90,20, this.game)
         this.add(teleporter)
+
+
     }
 }
--- src/js/rooms/poolRoom.js
+++ src/js/rooms/poolRoom.js
@@ -19,6 +19,33 @@ export class poolRoom extends room {
 
     }
     spawnBarriers() {
+        const barrierleft = new Barrier(154,430,308,860)
+        this.add(barrierleft)
+        const barrierright = new Barrier(1390,430,308,860)
+        this.add(barrierright)
+        const barriertop = new Barrier(765,48,1000,96)
+        this.add(barriertop)
 
+        const barrierpoolleft1 = new Barrier(550,350,20,180)
+        this.add(barrierpoolleft1)
+        const barrierpoolleft2 = new Barrier(550,660,20,180)
+        this.add(barrierpoolleft2)
+        const barrierpoolright1 = new Barrier(1020,350,20,180)
+        this.add(barrierpoolright1)
+        const barrierpoolright2 = new Barrier(1020,660,20,180)
+        this.add(barrierpoolright2)
+
+        
+        const barrierpooltop1 = new Barrier(630,270,180,20)
+        this.add(barrierpooltop1)
+        const barrierpooltop2 = new Barrier(920,270,180,20)
+        this.add(barrierpooltop2)
+        const barrierpoolbottom1 = new Barrier(630,740,180,20)
+        this.add(barrierpoolbottom1)
+        const barrierpoolbottom2 = new Barrier(920,740,180,20)
+        this.add(barrierpoolbottom2)
+        
+        const teleporter = new Tp(770,90,90,20, this.game)
+        this.add(teleporter)
     }
 }
