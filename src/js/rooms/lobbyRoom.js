c0d5b93c021f1ef9ee7835f9cbbbf6ec0cb868a1
RikWijtman <1045768@hr.nl>
2023-06-16 10:34:28 +0200

    room fix

--- src/js/rooms/lobbyRoom.js
+++ src/js/rooms/lobbyRoom.js
@@ -4,11 +4,28 @@ import { mainCharacter } from '../mainCharacter.js'
 import { ghoul } from '../enemies/ghoul.js'
 import { spirit } from '../enemies/spirit.js'
 import { Resources } from '../resources.js'
+import { Barrier } from '../ui/barrier.js'
+import { Tp } from '../ui/tp.js'
 
 import { room } from './room.js'
 
 export class lobbyRoom extends room {
     roomBackground = Resources.Lobby
     spawnBarriers() {
+        const barriertopleft = new Barrier(205,215,410,430)
+        this.add(barriertopleft)
+        const barriertop = new Barrier(980,40,1150,80)
+        this.add(barriertop)
+        const barrierbali1 = new Barrier(600,390,600,80)
+        this.add(barrierbali1)
+        const barrierbali2 = new Barrier(865,280,70,200)
+        this.add(barrierbali2)
+        const barrierfridge1 = new Barrier(20,650,40,200)
+        this.add(barrierfridge1)
+        const barrierfridge2 = new Barrier(1520,465,40,450)
+        this.add(barrierfridge2)
+        
+        const teleporter = new Tp(1290,80,90,20)
+        this.add(teleporter)
     }
 }
--- src/js/rooms/room.js
+++ src/js/rooms/room.js
@@ -5,8 +5,6 @@ import { ghoul } from '../enemies/ghoul.js'
 import { spirit } from '../enemies/spirit.js'
 import { Resources } from '../resources.js'
 import { roomBack } from './roomBack.js'
-import { Barrier } from '../ui/barrier.js'
-import { Tp } from '../ui/tp.js'
 
 export class room extends Scene {
     roomBackground
@@ -38,20 +36,6 @@ export class room extends Scene {
         this.spawnBarriers()
 
         
-        const barriertopleft = new Barrier(205,215,410,430)
-        this.add(barriertopleft)
-        const barriertop = new Barrier(980,40,1150,80)
-        this.add(barriertop)
-        const barrierbali1 = new Barrier(600,390,600,80)
-        this.add(barrierbali1)
-        const barrierbali2 = new Barrier(865,280,70,200)
-        this.add(barrierbali2)
-        const barrierfridge1 = new Barrier(20,650,40,200)
-        this.add(barrierfridge1)
-        const barrierfridge2 = new Barrier(1520,465,40,450)
-        this.add(barrierfridge2)
-        
-        const teleporter = new Tp(1290,80,90,20)
-        this.add(teleporter)
+
     }
 }
