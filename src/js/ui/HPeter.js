0b0edf1804518623f1ab6f5eb44029ef0f084b8e
RikWijtman <1045768@hr.nl>
2023-06-19 14:12:01 +0200

    hp

--- src/js/ui/HPeter.js
+++ src/js/ui/HPeter.js
@@ -2,13 +2,13 @@ import { Actor, Vector } from "excalibur"
 import * as ex from 'excalibur'
 import {Resources} from "../resources.js"
 
-export class Barrier extends Actor {
+export class HP extends Actor {
 
-    constructor(x,y,width,height) {
-        super({width:Resources.Barrier.width, height:Resources.Barrier.height})
-        this.graphics.use(Resources.Barrier.toSprite())
-        this.pos = new Vector(x,y)
-        this.scale = new Vector(width/512, height/512)
+    constructor() {
+        super({width:Resources.Hp3.width, height:Resources.Hp3.height})
+        this.graphics.use(Resources.Hp3.toSprite())
+        this.pos = new Vector(1000,800)
+        this.scale = new Vector(100/512, 100/512)
         this.graphics.opacity = 0
     }
 
