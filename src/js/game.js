083d52337061ccc9621d0aed0acd1c07e6164708
Matsdd <Matsdoornhein04@gmail.com>
2023-06-22 12:03:06 +0200

    bathroom clipping

--- src/js/game.js
+++ src/js/game.js
@@ -73,7 +73,7 @@ export class Game extends Engine {
         this.addScene('kitchen2', this.kitchen2)
         this.addScene('engine', this.engine)
 
-        this.goToScene('engine')
+        this.goToScene('bathroom')
 
         console.log("Enjoy!")
     }
