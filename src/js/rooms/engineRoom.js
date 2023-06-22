e3fef6a146dc8cc60ed3eca1aeed2769c6fc0b2b
Matsdd <Matsdoornhein04@gmail.com>
2023-06-22 11:21:15 +0200

    engine

--- src/js/rooms/engineRoom.js
+++ src/js/rooms/engineRoom.js
@@ -14,22 +14,38 @@ import { room } from './room.js'
 export class engineRoom extends room {
     roomBackground = Resources.Engine
     zwart
-    tpx = 767
-    tpy = 80
+    tpx = 1215
+    tpy = 70
     spawnBarriers() {
-        const up = new Barrier(767,80,950,10)
+        const up = new Barrier(767,70,1550,10)
         this.add(up)
-        const left = new Barrier(300,510,10,860)
-        this.add(left)
-        const right = new Barrier(1234,510,10,860)
-        this.add(right)
+        const leftPipe = new Barrier(270,380,40,425)
+        this.add(leftPipe)
+        const leftBarrel = new Barrier(210,365,100,155)
+        this.add(leftBarrel)
+        const rightPipe1 = new Barrier(1500,540,120,245)
+        this.add(rightPipe1)
+        const rightPipe2 = new Barrier(1395,638,120,45)
+        this.add(rightPipe2)
+        const rightPipe3 = new Barrier(1355,600,40,70)
+        this.add(rightPipe3)
+        const middleBlock = new Barrier(793,576,540,255)
+        this.add(middleBlock)
+        const topBlock = new Barrier(793,160,540,255)
+        this.add(topBlock)
+        const topPipe1 = new Barrier(1200,260,280,40)
+        this.add(topPipe1)
+        const topPipe2 = new Barrier(1315,220,45,40)
+        this.add(topPipe2)
+        const rightBarrel = new Barrier(1255,370,80,180)
+        this.add(rightBarrel)
     }
     onDeactivate() {
         this.Sjaak.kill()
     }
 
     onActivate() {
-        this.Sjaak = new mainCharacter(767, 800)
+        this.Sjaak = new mainCharacter(150, 800)
         this.add(this.Sjaak)
     }
 }
