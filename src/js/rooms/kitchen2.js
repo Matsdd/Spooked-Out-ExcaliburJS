80a28efd99ffd74db6330b188c05d56b74ff73ff
Matsdd <Matsdoornhein04@gmail.com>
2023-06-22 13:22:08 +0200

    kitchen2done

--- src/js/rooms/kitchen2.js
+++ src/js/rooms/kitchen2.js
@@ -14,15 +14,33 @@ import { room } from './room.js'
 export class kitchen2 extends room {
     roomBackground = Resources.kitchen2
     zwart
-    tpx = 767
+    tpx = 580
     tpy = 80
     spawnBarriers() {
         const up = new Barrier(767,80,950,10)
         this.add(up)
         const left = new Barrier(300,510,10,860)
         this.add(left)
+        const leftbottomshelf = new Barrier(335,645,80,440)
+        this.add(leftbottomshelf)
+        const lefttopshelf = new Barrier(335,210,80,300)
+        this.add(lefttopshelf)
+        const topchair = new Barrier(440,170,60,60)
+        this.add(topchair)
+        const cooking = new Barrier(598,630,145,370)
+        this.add(cooking)
         const right = new Barrier(1234,510,10,860)
         this.add(right)
+        const rightwall1 = new Barrier(1020,380,440,120)
+        this.add(rightwall1)
+        const rightwall2 = new Barrier(810,315,15,80)
+        this.add(rightwall2)
+        const rightwall3 = new Barrier(810,120,15,180)
+        this.add(rightwall3)
+        const shelves = new Barrier(1020,100,400,80)
+        this.add(shelves)
+        const boxes = new Barrier(1140,220,200,200)
+        this.add(boxes)
     }
     onDeactivate() {
         this.Sjaak.kill()
