384e9396b42a7ce4cfd9085db05053913f077469
SyGitHZ <sybreng@gmail.com>
2023-06-26 10:29:05 +0200

    wraith sounds

--- src/js/enemies/wraith.js
+++ src/js/enemies/wraith.js
@@ -37,6 +37,10 @@ export class wraith extends ghost {
 
     this.on('collisionstart', (event) => {
       const hitSound = new Audio(Resources.hitSound.path);
+      const ghostDeath1 = new Audio(Resources.ghostDeath1.path);
+      const ghostDeath2 = new Audio(Resources.ghostDeath2.path);
+      ghostDeath1.volume = 0.5
+      ghostDeath2.volume = 0.5
       hitSound.volume = 0.3;
       if (event.other instanceof bullet) {
         this.hp -= 1;
@@ -44,6 +48,17 @@ export class wraith extends ghost {
         hitSound.play();
         if (this.hp <= 0) {
           this.kill();
+          this.randomNumber
+          this.randomNumber = this.getRandomInt(2);
+
+          switch (this.randomNumber) {
+            case 0:
+              ghostDeath1.play();
+              break;
+            case 1:
+              ghostDeath2.play();
+              break;
+          }
         }
       }
       if (event.other instanceof mainCharacter) {
@@ -57,23 +72,23 @@ export class wraith extends ghost {
     switch (this.chosenPath) {
       case 0:
         this.maxDistance = 450,
-        this.path = [
-          new Vector(470, 600),
-          new Vector(470, 450),
-          new Vector(570, 450),
-          new Vector(570, 600),
-        ]
+          this.path = [
+            new Vector(470, 600),
+            new Vector(470, 450),
+            new Vector(570, 450),
+            new Vector(570, 600),
+          ]
         break;
       case 1:
         this.maxDistance = 450,
-        this.path = [
-          new Vector(1000, 350),
-          new Vector(400, 350),
-          new Vector(1000, 350),
-          new Vector(400, 350),
-        ]
+          this.path = [
+            new Vector(1000, 350),
+            new Vector(400, 350),
+            new Vector(1000, 350),
+            new Vector(400, 350),
+          ]
         break;
-  }
+    }
 
     // Start playing sounds at random intervals
     this.playSoundAtRandomInterval();
@@ -86,15 +101,43 @@ export class wraith extends ghost {
     const randomInterval = Math.random() * (maxInterval - minInterval) + minInterval;
 
     // Play the sound
+    // const sounds = new Audio[Resources.Ghost1.path, Resources.Ghost2.path, Resources.Ghost3.path];
+
     const sound = new Audio(Resources.Ghost1.path);
+    const sound2 = new Audio(Resources.Ghost2.path);
+    const sound3 = new Audio(Resources.Ghost3.path);
+    const sound4 = new Audio(Resources.Ghost4.path);
     sound.volume = 0.2;
+    sound2.volume = 0.2;
+    sound3.volume = 0.2;
+    sound4.volume = 0.2;
 
     // Set pitch
     const minPlaybackRate = 1; // Minimum playback rate
     const maxPlaybackRate = 4; // Maximum playback rate
     const randomPlaybackRate = Math.random() * (maxPlaybackRate - minPlaybackRate) + minPlaybackRate;
     sound.playbackRate = randomPlaybackRate;
-    sound.play();
+
+
+    this.randomNumber
+    this.randomNumber = this.getRandomInt(4);
+
+    switch (this.randomNumber) {
+      case 0:
+        sound.play();
+        break;
+      case 1:
+        sound2.play();
+        break;
+      case 2:
+        sound3.play();
+        break;
+      case 3:
+        sound4.play();
+        break;
+    }
+
+
 
     // Schedule the next sound playback
     this.soundInterval = setTimeout(() => {
@@ -106,33 +149,34 @@ export class wraith extends ghost {
     const direction = this.target.pos.sub(this.pos);
     const distance = direction.distance();
 
-    if (this.bounceTimer < 0) {    if (distance > this.minDistance && distance < this.maxDistance || this.aggro === true) {
-      const desiredVel = direction.normalize().scale(this.speed);
-      this.vel = desiredVel.clampMagnitude(this.speed);
+    if (this.bounceTimer < 0) {
+      if (distance > this.minDistance && distance < this.maxDistance || this.aggro === true) {
+        const desiredVel = direction.normalize().scale(this.speed);
+        this.vel = desiredVel.clampMagnitude(this.speed);
 
-      // Calculate rotation based on movement direction
-      this.rotation = Math.atan2(this.vel.y, this.vel.x);
-    } else {
-    // Follow the predefined path
-    const targetWaypoint = this.path[this.currentWaypoint];
-    const direction = targetWaypoint.sub(this.pos);
-    const distance = direction.distance();
+        // Calculate rotation based on movement direction
+        this.rotation = Math.atan2(this.vel.y, this.vel.x);
+      } else {
+        // Follow the predefined path
+        const targetWaypoint = this.path[this.currentWaypoint];
+        const direction = targetWaypoint.sub(this.pos);
+        const distance = direction.distance();
 
-    if (distance > this.minDistance) {
-      const desiredVel = direction.normalize().scale(this.speed);
-      this.vel = desiredVel.clampMagnitude(this.speed);
+        if (distance > this.minDistance) {
+          const desiredVel = direction.normalize().scale(this.speed);
+          this.vel = desiredVel.clampMagnitude(this.speed);
 
-      // Calculate rotation based on movement direction
-      this.rotation = Math.atan2(this.vel.y, this.vel.x);
-    } else {
-      // Reached the current waypoint, move to the next one
-      this.currentWaypoint = (this.currentWaypoint + 1) % this.path.length;
-      this.vel = Vector.Zero;
+          // Calculate rotation based on movement direction
+          this.rotation = Math.atan2(this.vel.y, this.vel.x);
+        } else {
+          // Reached the current waypoint, move to the next one
+          this.currentWaypoint = (this.currentWaypoint + 1) % this.path.length;
+          this.vel = Vector.Zero;
+        }
+      }
     }
+
   }
-    }
-
-}
 
   update(engine, delta) {
     this.bounceTimer -= 1
@@ -142,7 +186,7 @@ export class wraith extends ghost {
       const targetWaypoint = this.path[this.currentWaypoint];
       this.moveTowardsTarget(targetWaypoint);
     }
-  
+
     // Call the base update method to apply the calculated velocity and rotation
     super.update(engine, delta);
   }
