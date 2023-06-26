4c1b4194c5fdc6404beae1c1011f9f99e944858c
SyGitHZ <sybreng@gmail.com>
2023-06-26 10:20:10 +0200

    sounds

--- src/js/enemies/phantom.js
+++ src/js/enemies/phantom.js
@@ -30,31 +30,47 @@ export class phantom extends ghost {
     ]
     this.currentWaypoint = 0;
     this.invTimer = 0;
-    this.graphics.opacity = 1; 
+    this.graphics.opacity = 1;
   }
 
   onInitialize() {
 
     this.on('collisionstart', (event) => {
       const hitSound = new Audio(Resources.hitSound.path);
+      const ghostDeath1 = new Audio(Resources.ghostDeath1.path);
+      const ghostDeath2 = new Audio(Resources.ghostDeath2.path);
+      ghostDeath1.volume = 0.5
+      ghostDeath2.volume = 0.5
       hitSound.volume = 0.3;
       if (this.invTimer <= 0) {
         if (event.other instanceof bullet) {
-            this.hp -= 1;
-            this.invTimer = 100;
-            hitSound.play();
-            if (this.hp <= 0) {
+          this.hp -= 1;
+          this.invTimer = 100;
+          hitSound.play();
+          if (this.hp <= 0) {
             this.kill();
+            this.randomNumber
+            this.randomNumber = this.getRandomInt(2);
+
+            switch (this.randomNumber) {
+              case 0:
+                ghostDeath1.play();
+                break;
+              case 1:
+                ghostDeath2.play();
+                break;
             }
+
+          }
         }
-    }
-        if (event.other instanceof mainCharacter) {
-            this.vel = new Vector(
-            Math.cos(this.rotation) * -10 * this.speed,
-            Math.sin(this.rotation) * -10 * this.speed
-            )
-            this.bounceTimer = 10
-        }
+      }
+      if (event.other instanceof mainCharacter) {
+        this.vel = new Vector(
+          Math.cos(this.rotation) * -10 * this.speed,
+          Math.sin(this.rotation) * -10 * this.speed
+        )
+        this.bounceTimer = 10
+      }
     });
 
 
@@ -70,14 +86,37 @@ export class phantom extends ghost {
 
     // Play the sound
     const sound = new Audio(Resources.Ghost1.path);
+    const sound2 = new Audio(Resources.Ghost2.path);
+    const sound3 = new Audio(Resources.Ghost3.path);
+    const sound4 = new Audio(Resources.Ghost4.path);
     sound.volume = 0.2;
+    sound2.volume = 0.2;
+    sound3.volume = 0.2;
+    sound4.volume = 0.2;
 
     // Set pitch
     const minPlaybackRate = 1; // Minimum playback rate
     const maxPlaybackRate = 4; // Maximum playback rate
     const randomPlaybackRate = Math.random() * (maxPlaybackRate - minPlaybackRate) + minPlaybackRate;
     sound.playbackRate = randomPlaybackRate;
-    sound.play();
+
+    this.randomNumber
+    this.randomNumber = this.getRandomInt(4);
+
+    switch (this.randomNumber) {
+      case 0:
+        sound.play();
+        break;
+      case 1:
+        sound2.play();
+        break;
+      case 2:
+        sound3.play();
+        break;
+      case 3:
+        sound4.play();
+        break;
+    }
 
     // Schedule the next sound playback
     this.soundInterval = setTimeout(() => {
@@ -89,41 +128,42 @@ export class phantom extends ghost {
     const direction = this.target.pos.sub(this.pos);
     const distance = direction.distance();
 
-    if (this.bounceTimer < 0) {    if (distance > this.minDistance && distance < this.maxDistance) {
-      const desiredVel = direction.normalize().scale(this.speed);
-      this.vel = desiredVel.clampMagnitude(this.speed);
+    if (this.bounceTimer < 0) {
+      if (distance > this.minDistance && distance < this.maxDistance) {
+        const desiredVel = direction.normalize().scale(this.speed);
+        this.vel = desiredVel.clampMagnitude(this.speed);
 
-      // Calculate rotation based on movement direction
-      this.rotation = Math.atan2(this.vel.y, this.vel.x);
-    } else {
-    // Follow the predefined path
-    const targetWaypoint = this.path[this.currentWaypoint];
-    const direction = targetWaypoint.sub(this.pos);
-    const distance = direction.distance();
+        // Calculate rotation based on movement direction
+        this.rotation = Math.atan2(this.vel.y, this.vel.x);
+      } else {
+        // Follow the predefined path
+        const targetWaypoint = this.path[this.currentWaypoint];
+        const direction = targetWaypoint.sub(this.pos);
+        const distance = direction.distance();
 
-    if (distance > this.minDistance) {
-      const desiredVel = direction.normalize().scale(this.speed);
-      this.vel = desiredVel.clampMagnitude(this.speed);
+        if (distance > this.minDistance) {
+          const desiredVel = direction.normalize().scale(this.speed);
+          this.vel = desiredVel.clampMagnitude(this.speed);
 
-      // Calculate rotation based on movement direction
-      this.rotation = Math.atan2(this.vel.y, this.vel.x);
-    } else {
-      // Reached the current waypoint, move to the next one
-      this.currentWaypoint = (this.currentWaypoint + 1) % this.path.length;
-      this.vel = Vector.Zero;
+          // Calculate rotation based on movement direction
+          this.rotation = Math.atan2(this.vel.y, this.vel.x);
+        } else {
+          // Reached the current waypoint, move to the next one
+          this.currentWaypoint = (this.currentWaypoint + 1) % this.path.length;
+          this.vel = Vector.Zero;
+        }
+      }
     }
+
   }
-    }
-
-}
 
   update(engine, delta) {
     if (this.invTimer > 0) {
-        this.invTimer--
-        this.graphics.opacity = 0.2
+      this.invTimer--
+      this.graphics.opacity = 0.2
     }
-    if (this.invTimer <= 0){
-        this.graphics.opacity = 1
+    if (this.invTimer <= 0) {
+      this.graphics.opacity = 1
     }
 
     this.bounceTimer -= 1
@@ -133,7 +173,7 @@ export class phantom extends ghost {
       const targetWaypoint = this.path[this.currentWaypoint];
       this.moveTowardsTarget(targetWaypoint);
     }
-  
+
     // Call the base update method to apply the calculated velocity and rotation
     super.update(engine, delta);
   }
