ff5796b4106a6f34fe14765e1a65a1b31e194a86
RikWijtman <1045768@hr.nl>
2023-06-27 14:31:01 +0200

    stof

--- src/js/ui/STOOFZUGER.js
+++ src/js/ui/STOOFZUGER.js
@@ -5,10 +5,14 @@ import {Resources} from "../resources.js"
 export class Stof extends Actor {
 
     constructor() {
-        super({width:Resources.Barrier.width, height:Resources.Barrier.height})
-        this.graphics.use(Resources.Barrier.toSprite())
+        super({width:Resources.Stofzuiger.width, height:Resources.Stofzuiger.height})
+        this.graphics.use(Resources.Stofzuiger.toSprite())
         this.pos = new Vector(765,430)
-        this.scale = new Vector(1,1)
-        this.graphics.opacity = 1
+        this.scale = new Vector(1.3,1.3)
+        this.graphics.opacity = 0
+    }
+
+    update() {
+        this.kill()
     }
 }
