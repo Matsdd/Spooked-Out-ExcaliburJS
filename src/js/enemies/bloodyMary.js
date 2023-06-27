8473202901d6f0e13349982511de6a7b2d10e3bf
lindsey <lindseytaak123@gmail.com>
2023-06-27 15:46:40 +0200

    bloody mary

--- src/js/enemies/bloodyMary.js
+++ src/js/enemies/bloodyMary.js
@@ -53,15 +53,15 @@ export class bloodyMary extends ghost {
     const currentScene = engine.currentScene;
 
     this.on('collisionstart', (event) => {
-      const hitSound = new Audio(Resources.hitSound.path);
-      const ghostDeath1 = new Audio(Resources.ghostDeath1.path);
-      const ghostDeath2 = new Audio(Resources.ghostDeath2.path);
-      ghostDeath1.volume = 0.5
-      ghostDeath2.volume = 0.5
-      hitSound.volume = 0.3;
+      // const hitSound = new Audio(Resources.hitSound.path);
+      // const ghostDeath1 = new Audio(Resources.ghostDeath1.path);
+      // const ghostDeath2 = new Audio(Resources.ghostDeath2.path);
+      // ghostDeath1.volume = 0.5
+      // ghostDeath2.volume = 0.5
+      // hitSound.volume = 0.3;
       if (event.other instanceof bullet) {
         this.hp -= 1;
-        hitSound.play();
+        // hitSound.play();
         if (this.hp <= 0) {
           this.kill();
           this.randomNumber
@@ -89,16 +89,16 @@ export class bloodyMary extends ghost {
               }
             break;
           }
-          this.randomNumber = this.getRandomInt(2);
+          // this.randomNumber = this.getRandomInt(2);
 
-          switch (this.randomNumber) {
-            case 0:
-              ghostDeath1.play();
-              break;
-            case 1:
-              ghostDeath2.play();
-              break;
-          }
+          // switch (this.randomNumber) {
+          //   case 0:
+          //     ghostDeath1.play();
+          //     break;
+          //   case 1:
+          //     ghostDeath2.play();
+          //     break;
+          // }
 
         }
       }
@@ -124,59 +124,59 @@ export class bloodyMary extends ghost {
     engine.currentScene.add(bossbar)
 
     // Start playing sounds at random intervals
-    this.playSoundAtRandomInterval();
+  //   this.playSoundAtRandomInterval();
   }
 
-  playSoundAtRandomInterval() {
-    const minInterval = 8000; // Minimum interval in milliseconds
-    const maxInterval = 17000; // Maximum interval in milliseconds
+  // playSoundAtRandomInterval() {
+  //   const minInterval = 8000; // Minimum interval in milliseconds
+  //   const maxInterval = 17000; // Maximum interval in milliseconds
 
-    const randomInterval = Math.random() * (maxInterval - minInterval) + minInterval;
+  //   const randomInterval = Math.random() * (maxInterval - minInterval) + minInterval;
 
-    // Play the sound
-    // const sounds = new Audio[Resources.Ghost1.path, Resources.Ghost2.path, Resources.Ghost3.path];
+  //   // Play the sound
+  //   // const sounds = new Audio[Resources.Ghost1.path, Resources.Ghost2.path, Resources.Ghost3.path];
 
-    const sound = new Audio(Resources.Ghost1.path);
-    const sound2 = new Audio(Resources.Ghost2.path);
-    const sound3 = new Audio(Resources.Ghost3.path);
-    const sound4 = new Audio(Resources.Ghost4.path);
-    sound.volume = 0.2;
-    sound2.volume = 0.2;
-    sound3.volume = 0.2;
-    sound4.volume = 0.2;
+  //   const sound = new Audio(Resources.Ghost1.path);
+  //   const sound2 = new Audio(Resources.Ghost2.path);
+  //   const sound3 = new Audio(Resources.Ghost3.path);
+  //   const sound4 = new Audio(Resources.Ghost4.path);
+  //   sound.volume = 0.2;
+  //   sound2.volume = 0.2;
+  //   sound3.volume = 0.2;
+  //   sound4.volume = 0.2;
 
-    // Set pitch
-    const minPlaybackRate = 1; // Minimum playback rate
-    const maxPlaybackRate = 4; // Maximum playback rate
-    const randomPlaybackRate = Math.random() * (maxPlaybackRate - minPlaybackRate) + minPlaybackRate;
-    sound.playbackRate = randomPlaybackRate;
-
-
-    this.randomNumber
-    this.randomNumber = this.getRandomInt(4);
-
-    switch (this.randomNumber) {
-      case 0:
-        sound.play();
-        break;
-      case 1:
-        sound2.play();
-        break;
-      case 2:
-        sound3.play();
-        break;
-      case 3:
-        sound4.play();
-        break;
-    }
+  //   // Set pitch
+  //   const minPlaybackRate = 1; // Minimum playback rate
+  //   const maxPlaybackRate = 4; // Maximum playback rate
+  //   const randomPlaybackRate = Math.random() * (maxPlaybackRate - minPlaybackRate) + minPlaybackRate;
+  //   sound.playbackRate = randomPlaybackRate;
 
 
+  //   this.randomNumber
+  //   this.randomNumber = this.getRandomInt(4);
 
-    // Schedule the next sound playback
-    this.soundInterval = setTimeout(() => {
-      this.playSoundAtRandomInterval();
-    }, randomInterval);
-  }
+  //   switch (this.randomNumber) {
+  //     case 0:
+  //       sound.play();
+  //       break;
+  //     case 1:
+  //       sound2.play();
+  //       break;
+  //     case 2:
+  //       sound3.play();
+  //       break;
+  //     case 3:
+  //       sound4.play();
+  //       break;
+  //   }
+
+
+
+  //   // Schedule the next sound playback
+  //   this.soundInterval = setTimeout(() => {
+  //     this.playSoundAtRandomInterval();
+  //   }, randomInterval);
+  // }
 
   moveTowardsTarget(target) {
     const direction = this.target.pos.sub(this.pos);
@@ -280,7 +280,7 @@ export class bloodyMary extends ghost {
 
   onPostKill() {
     // Clear the sound interval
-    clearTimeout(this.soundInterval);
+    // clearTimeout(this.soundInterval);
     this.game.addScore(15,false)
   }
 
