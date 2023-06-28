6275d80e067ad34271127fa0de98d69532daf0b2
lindsey <lindseytaak123@gmail.com>
2023-06-28 10:33:00 +0200

    drops

--- src/js/props/treasure.js
+++ src/js/props/treasure.js
@@ -4,10 +4,16 @@ import { mainCharacter } from '../mainCharacter.js'
 import { mimic } from '../enemies/mimic.js'
 import { Barrier } from '../ui/barrier.js';
 import { bullet } from '../bullet.js'
+import { bril } from "../hoeden/bril.js";
+import { duikbril } from "../hoeden/duikbril.js";
+import { clown } from "../hoeden/clown.js";
+import { plaag } from "../hoeden/plaag.js";
+import { cowboy } from "../hoeden/cowboy.js";
+import { mini } from "../hoeden/mini.js";
 
 export class treasure extends Actor {
   constructor(target, posX, posY,game) {
-    super({ width: Resources.Treasure.width / 1.8, height: Resources.Treasure.height / 3});
+    super({ width: Resources.Treasure.width / 1.6, height: Resources.Treasure.height / 3});
     this.pos = new Vector(posX, posY);
     this.game = game
     this.target = target;
@@ -18,7 +24,7 @@ export class treasure extends Actor {
     this.scale = new Vector(0.4, 0.4);
     const currentScene = Engine.currentScene;
 
-    const treasure1 = new Barrier(this.pos.x, this.pos.y, Resources.Treasure.width /4,Resources.Treasure.height / 8)
+    const treasure1 = new Barrier(this.pos.x, this.pos.y, Resources.Treasure.width /4.2,Resources.Treasure.height / 8)
     currentScene.add(treasure1);
 
     this.on('collisionstart', (event) => {
@@ -30,16 +36,32 @@ export class treasure extends Actor {
           currentScene.add(Mimic);
           this.kill();
         } else {
-        this.randomNumber = this.getRandomInt(3);
+        this.randomNumber = this.getRandomInt(6);
         switch (this.randomNumber) {
-          case 0:
-            this.Hoed = new mimic(this.target, this.pos.x, this.pos.y, this.game);
-            currentScene.add(this.Mimic);
-            break;
-          case 1:
-            this.Hoed = new mimic(this.target, this.pos.x, this.pos.y, this.game);
-            currentScene.add(this.Mimic);
-            break;
+            case 0:
+              this.Hoed = new bril(this.pos.x, this.pos.y);
+              currentScene.add(this.Hoed);
+              break;
+            case 1:
+              this.Hoed = new duikbril(this.pos.x, this.pos.y);
+              currentScene.add(this.Hoed);
+              break;
+            case 2:
+              this.Hoed = new clown(this.pos.x, this.pos.y);
+              currentScene.add(this.Hoed);
+              break;
+            case 3:
+              this.Hoed = new plaag(this.pos.x, this.pos.y);
+              currentScene.add(this.Hoed);
+              break;
+            case 4:
+              this.Hoed = new cowboy(this.pos.x, this.pos.y);
+              currentScene.add(this.Hoed);
+              break;
+            case 5:
+              this.Hoed = new mini(this.pos.x, this.pos.y);
+              currentScene.add(this.Hoed);
+              break;
         }
         this.kill();
         }
